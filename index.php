4e1c7002a0c4fa4c6a9c2f14a1c7f8470bc146c5
Louis Pinoit <louis@pinoit.com>
2023-11-07 23:55:16 +0100

    Changes

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 
 // Modèle sur lequel on va se baser afin d'instancier des objets
 
+// Appeler un attribut => $objet->attribut
+// Appeler une méthode => $objet->methode()
+
 class Chat {
     
     public string $nom;
@@ -20,7 +23,20 @@ class Chat {
 
 
 
-$chat = new Chat();
+$chat1 = new Chat();
+$chat1->nom = "Gribouille";
+
+echo "Nom : " . $chat1->nom;
+echo "<br/>";
+$chat1->miaou();
+echo "<br/>";
 
 
+//ne pas cloner avec $chat2 = $chat1 (copie conforme de l'objet)
+$chat2 = clone $chat1;
+
+$chat1->nom = "Georges";
+echo "<pre>";
+var_dump($chat1, $chat2);
+echo "</pre>";
 
