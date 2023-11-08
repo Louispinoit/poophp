6f1418ce94d4029fb927520181b74d18f7ee08aa
Louis Pinoit <louis@pinoit.com>
2023-11-08 02:22:46 +0100

    Constructeur

--- index.php
+++ index.php
@@ -8,11 +8,20 @@
 // Appeler une méthode => $objet->methode()
 
 // Modificateur de visibilité
-// 1 - public
+// 1 - public (modifier / acceder depuis n'importe où<)
 // 2 - private (modifier / acceder un attribut uniquement depuis la classe.)
 // 3 - protected (permet de modifier / acceder depuis la classe et classe enfant. )
+// => permet d'éviter les erreurs sur le long terme.
+// => sert à l'encapsulation
+
+// Contructeur => Permet de faire des actions lors de l'instanciation de la classe.
 
 class Chat {
+
+    
+
+    //constante de classe 
+    public const AGE_MINIMUM = 1;
     
     public string $nom;
     
@@ -20,21 +29,27 @@ class Chat {
 
     public ?string $race = null;
 
+    //Methode magique
+    public function __construct(string $nom)
+    {
+        var_dump($this);
+    }
+
     public function miaou(): void 
     {
         echo "Miaou" ;
     }
+
+  
 }
 
 
+$nomChat = "Gribouille";
 
-$chat1 = new Chat();
-$chat1->nom = "Gribouille";
+$chat1 = new Chat($nomChat);
 
-echo "Nom : " . $chat1->nom;
-echo "<br/>";
-$chat1->miaou();
-echo "<br/>";
+
+
 
 
 //ne pas cloner avec $chat2 = $chat1 (copie conforme de l'objet)
@@ -44,4 +59,5 @@ $chat1->nom = "Georges";
 echo "<pre>";
 var_dump($chat1, $chat2);
 echo "</pre>";
+echo Chat::AGE_MINIMUM;
 
